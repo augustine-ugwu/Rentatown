4666f2ff9c3cbc6581863a554e284280f17680aa
augustine-ugwu <augustinehills@yahoo.com>
2024-05-22 12:53:03 +0100

    Routing Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./routes/HomePage.jsx";
+import Contact from "./routes/Contact.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./routes/NotFoundPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+  return (
+    <div>
+      <div>404 | Not Found</div>
+    </div>
+  );
+}
+
+export default NotFoundPage;
