fdceaf7ddd1887def82ac5e96fc834658fdda5bc
augustine-ugwu <augustinehills@yahoo.com>
2024-05-22 18:04:44 +0100

    Modified Homepage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,9 +1,79 @@
-function HomePage() {
+import * as React from "react";
+import PropTypes from "prop-types";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import AppAppBar from "../assets/components/AppAppBar";
+import Hero from "../assets/components/Hero";
+// import LogoCollection from "../assets/components/LogoCollection";
+import Highlights from "../assets/components/Highlights";
+// import Pricing from "../assets/components/Pricing";
+import Features from "../assets/components/Features";
+import Testimonials from "../assets/components/Testimonials";
+import FAQ from "../assets/components/FAQ";
+import Footer from "../assets/components/Footer";
+import Theme from "../assets/components/Theme";
+
+function ToggleCustomTheme() {
   return (
-    <>
-      <h1>HomePage</h1>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100dvw",
+        position: "fixed",
+        bottom: 24,
+      }}></Box>
   );
 }
 
-export default HomePage;
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+};
+
+export default function LandingPage() {
+  const [mode, setMode] = React.useState("light");
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(Theme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
+  return (
+    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
+      <Box sx={{ bgcolor: "background.default" }}>
+        {/* <LogoCollection /> */}
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        {/* <Pricing /> */}
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+      <ToggleCustomTheme
+        showCustomTheme={showCustomTheme}
+        toggleCustomTheme={toggleCustomTheme}
+      />
+    </ThemeProvider>
+  );
+}
